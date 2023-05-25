3ed57b951daf89e76b944374273aa8c78c75aeeb
Borovyk-Mykola <114910601+Borovyk-Mykola@users.noreply.github.com>
2023-05-25 20:25:35 +0300

    111

--- src/components/ImageGallery/ImageGallery.js
+++ src/components/ImageGallery/ImageGallery.js
@@ -1,31 +1,49 @@
 import React from "react";
+import PropTypes from 'prop-types';
 import ImageGalleryItem from "components/ImageGalleryItem/ImageGalleryItem";
 import { Pixabay } from '../Pixabay'
 import Loader from '../Loader/Loader'
 import Button from '../Button/Button'
 
+const Status = {
+  IDLE: 'idle',
+  PENDING: 'pending',
+  RESOLVED: 'resolved',
+  REJECTED: 'rejected',
+};
 
 class ImageGallery extends React.Component {
+
+  static propTypes = {
+    value: PropTypes.string.isRequired,
+  };
+
   state = {
+    value: '',
     images: [],
     page: 0,
-    loader: false,
+    stutus: Status.IDLE,
   }
 
   componentDidUpdate(prevProps, prevState) {
-    if(prevProps.value !== this.props.value)
+    if(prevProps.value !== this.props.value 
+      && this.state.images !== [])
     this.addImage()
   }
 
   addImage() {
-    this.setState({page: 1, loader: true})
+    this.setState({page: 1, stutus: Status.PENDING, 
+      value: this.props.value})
   
     const pixabay = new Pixabay();
     pixabay.query = this.props.value.trim().toLowerCase();
     
     pixabay.getPhotos().then(response =>
-      {const res = response.hits;
-      this.setState({images: [...res], loader: false})
+      {const result = response.hits;
+      if(result.length === 0)
+        {alert('Enter valid text to search!')
+        this.setState({page: 0, stutus: Status.IDLE, value: ''})}
+      else this.setState({images: [...result], stutus: Status.RESOLVED})
       }
     )
   }
@@ -33,42 +51,45 @@ class ImageGallery extends React.Component {
   onClick = e => {
     e.preventDefault();
     const {page, images} = this.state
-    
-    this.setState({page: page + 1, loader: true})
-
     const pixabay = new Pixabay();
+    
+    if(pixabay.hasMorePhotos()){
+    this.setState({page: page + 1, stutus: Status.PENDING})
+
     pixabay.page = page + 1;
     pixabay.query = this.props.value.trim().toLowerCase();
     
     pixabay.getPhotos().then(response =>
-      {const res = response.hits;
-      this.setState({images: [...images, ...res],
-        loader: false})
+      {const result = response.hits;
+      this.setState({images: [...images, ...result],
+        stutus: Status.RESOLVED})
       }
-    )
+    )}
+    else 
+    alert(`We're sorry, but you've reached the end of search results ${this.props.value}`)
   }
 
   render() {
-    const { images, page, loader} = this.state;
-   
-    if(page === 0 && !loader) this.addImage()
-    return (
-    <div>
-      <ul className = "ImageGallery" >
-      {images !== [] && !loader && images.map(image => (
-        <ImageGalleryItem 
-        key={image.id}
-        largeImageURL={image.largeImageURL} 
-        webformatURL={image.webformatURL} 
-        tags={image.tags} 
-        />))}
-      </ul>
-      <div style={{textAlign: 'center'}}>
-        {images !== [] && !loader && <Button onClick={this.onClick}/>}
-        {loader && <Loader/>}
-      </div>
-    </div>
-    )
+    const {value, images, status} = this.state;
+    if(status === 'idle' && value !== '') this.addImage()
+    if(status === 'resolved'){
+      return (
+        <div>
+          <ul className = "ImageGallery" >
+          {images.map(image => (
+            <ImageGalleryItem 
+              key={image.id}
+              largeImageURL={image.largeImageURL} 
+              webformatURL={image.webformatURL} 
+              tags={image.tags} 
+            />))
+          }
+          </ul>
+          <Button onClick={this.onClick}/>
+        </div>
+    )}
+    if(status === 'pending')
+    {return <Loader/>}
   }
 }
 
